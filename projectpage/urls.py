109ea70e96dfad8ecef391f1a6057372e9a033fa
Spencer Cook <njw2wd@virginia.edu>
2024-10-03 10:00:31 -0400

    html pages
    Added some baselines for the login pages and dashboard

--- projectpage/urls.py
+++ projectpage/urls.py
@@ -5,5 +5,7 @@ from . import views
 app_name = "projectpage"
 urlpatterns = [
     path("", views.index, name="index"),
+    path("login/", views.login, name="login"),
+    path("dashboard/", views.dashboard, name="dashboard"), 
     path('accounts/', include('allauth.urls')),
 ]
--- projectpage/views.py
+++ projectpage/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'projectpage/index.html')
+
+def login(request):
+    return render(request, 'projectpage/login.html')
+
+def dashboard(request):
+    return render(request, 'projectpage/dashboard.html')
