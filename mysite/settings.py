1ccbbb75800248b11edd7ad587bd1f11314ba086
Spencer Cook <njw2wd@virginia.edu>
2024-10-17 11:50:54 -0400

    Settings of Amazon S3
    TODO: need to update .env file

--- mysite/settings.py
+++ mysite/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     #'allauth.socialaccount',
     #'allauth.socialaccount.providers.google',
     'social_django',
+    'storages',
 ]
 
 SITE_ID = 1
@@ -178,3 +179,15 @@ try:
         django_heroku.settings(locals())
 except ImportError:
     found = False
+
+# AWS info
+AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
+AWS_S3_REGION_NAME = os.getenv('AWS_S3_REGION_NAME', default='us-west-1')  # Change region if needed
+AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+
+# Settings for uploaded files
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+AWS_S3_FILE_OVERWRITE = False  # Prevent file overwrite with same name
+AWS_DEFAULT_ACL = None  # Make files public by default, remove if not needed
