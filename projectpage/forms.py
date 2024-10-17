ca2e1a3b91ad35aea46adf3b36ee54c7e0bf5945
Spencer Cook <njw2wd@virginia.edu>
2024-10-17 11:56:43 -0400

    Forms and models for AWS S3

--- projectpage/forms.py
+++ projectpage/forms.py
@@ -1,10 +1,14 @@
 from django import forms
 from django.forms import inlineformset_factory
 
-from .models import Task
+from .models import Task, Document
 
 class TaskForm(forms.ModelForm):
     class Meta:
         model = Task
         fields = ["task_title", "deadline"]
 
+class DocumentForm(forms.ModelForm):
+    class Meta:
+        model = Document
+        fields = ('file',)
--- projectpage/models.py
+++ projectpage/models.py
@@ -6,3 +6,7 @@ class Task(models.Model):
 
     def __str__(self):
         return f"{self.task_title}: Due on {self.deadline}"
+
+class Document(models.Model):
+    file = models.FileField(upload_to='documents/')
+    uploaded_at = models.DateTimeField(auto_now_add=True)
