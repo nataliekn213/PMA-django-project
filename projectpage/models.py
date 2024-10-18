19e0ab5ae9a0988b04ce300a9fb9bf9b720a8c6a
iuriv <iurivintonyak@gmail.com>
2024-10-18 16:35:01 -0400

    Fixed filestorage not being overwritten to S3

--- projectpage/models.py
+++ projectpage/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from storages.backends.s3boto3 import S3Boto3Storage
 
 class Task(models.Model):
     task_title = models.CharField(max_length=150)
@@ -8,5 +9,5 @@ class Task(models.Model):
         return f"{self.task_title}: Due on {self.deadline}"
 
 class Document(models.Model):
-    file = models.FileField(upload_to='documents/')
+    file = models.FileField(storage=S3Boto3Storage(), upload_to='documents/')
     uploaded_at = models.DateTimeField(auto_now_add=True)
