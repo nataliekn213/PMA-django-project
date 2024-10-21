7953f9bff88ae447dc7b157ba64a1c9d2dbc175a
Aarfdark <aadarsh.natarajan@gmail.com>
2024-10-21 17:06:11 -0400

    admin tests

--- projectpage/tests.py
+++ projectpage/tests.py
@@ -17,6 +17,26 @@ class TaskCreation(TestCase):
         self.assertTemplateUsed(response, "projectpage/task_list.html")
         self.assertContains(response, "test_title")
     
+class AdminStuff(TestCase):
+    def setUp(self):
+        self.admin = User.objects.create_superuser("admin", "test_admin@test.com", "admin_password")
+        self.non_admin = User.objects.create_user("non_admin", "test_non_admin@test.com", "non_admin_password")
+    
+    def test_non_admin_dashboard(self):
+        non_admin_login = self.client.login(username="non_admin", password="non_admin_password")
+        self.assertTrue(non_admin_login)
+
+        response = self.client.get(reverse_lazy("projectpage:admin_dashboard"))
+        self.assertEqual(response.status_code, 403)
+
+    def test_admin_dashboard(self):
+        admin_login = self.client.login(username="admin", password="admin_password")
+        self.assertTrue(admin_login)
+
+        response = self.client.get(reverse_lazy("projectpage:admin_dashboard"))
+        self.assertEqual(response.status_code, 200)
+
 class TaskDeletion(TestCase):
     def setUp(self):
         self.admin = User.objects.create_superuser("admin", "test@test.com", "password")
+        task = Task.objects.create(task_title="test_title", deadline=datetime.datetime.today())
