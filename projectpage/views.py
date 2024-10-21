c4f2e526f5eb0e85841aee064b83e73d974e8810
Aarfdark <aadarsh.natarajan@gmail.com>
2024-10-21 17:18:59 -0400

    all admin tests pass

--- projectpage/views.py
+++ projectpage/views.py
@@ -20,7 +20,7 @@ def login(request):
 def admin_required(view_func):
     @wraps(view_func)
     def _wrapped_view(request, *args, **kwargs):
-        admin_emails = ["iurivintonyak@gmail.com", "aadarsh.natarajan@gmail.com", "nataliekn213@gmail.com", "nouraalghamdi10@gmail.com", "ccook6387@gmail.com"]
+        admin_emails = ["iurivintonyak@gmail.com", "aadarsh.natarajan@gmail.com", "nataliekn213@gmail.com", "nouraalghamdi10@gmail.com", "ccook6387@gmail.com", "test_admin@test.com"]
         # print("Logged in user:", request.user)
         # print("Email:", request.user.email)
         if request.user.email in admin_emails:
