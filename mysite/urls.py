df8a2f96ec6b51d934a872823818f4aaad4662c0
Aarfdark <aadarsh.natarajan@gmail.com>
2024-12-03 16:19:04 -0500

    fixed redirect

--- mysite/urls.py
+++ mysite/urls.py
@@ -17,9 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 from django.contrib.auth import views as auth_views
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('projectpage/', include('projectpage.urls')),
+    path('', RedirectView.as_view(url="projectpage/", permanent=True)),
     path('admin/', admin.site.urls),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('login/', auth_views.LoginView.as_view(), name="login"),
